f38f7aa473bb1bb0a90fbd0739e68feb7588556f
pvince33 <52060037+pvince33@users.noreply.github.com>
2019-07-18 21:50:29 -0600

    rebuild OLTP

--- AlphaInvestments ERD Build.sql
+++ AlphaInvestments ERD Build.sql
@@ -1,15 +1,15 @@
 -- 
 -----------------------------------------------------------
 IF NOT EXISTS(SELECT * FROM sys.databases
-	WHERE name = N'AlphaInvestments')
-	CREATE DATABASE AlphaInvestments
+	WHERE name = N'AlphaInvestmentsFinal')
+	CREATE DATABASE AlphaInvestmentsFinal
 GO
-USE AlphaInvestments
+USE AlphaInvestmentsFinal
 --
 -- Alter the path so the script can find the CSV files 
 --
 DECLARE @data_path NVARCHAR(256);
-SELECT @data_path = 'C:\Users\mrroy\OneDrive\Documents\GitHub\AlphaInvestments\';
+SELECT @data_path = 'C:\Users\mrroy\OneDrive\Documents\GitHub\AlphaInvestmentsFinal\';
 --
 -- Delete existing tables
 --
@@ -19,20 +19,6 @@ IF EXISTS(
 	WHERE name = N'Sales_Transactions'
 	)
 	DROP TABLE Sales_Transactions
---
-IF EXISTS(
-	SELECT *
-	FROM sys.tables
-	WHERE name = N'Agent_Transactions'
-	)
-	DROP TABLE Agent_Transactions
-
-IF EXISTS(
-	SELECT *
-	FROM sys.tables
-	WHERE name = N'Card_Inventory'
-	)
-	DROP TABLE Card_Inventory
 --
 IF EXISTS(
 	SELECT *
@@ -84,8 +70,22 @@ IF EXISTS(
 	)
 	DROP TABLE Customer_List
 --
+IF EXISTS(
+	SELECT *
+	FROM sys.tables
+	WHERE name = N'Corporation_list'
+	)
+	DROP TABLE Corporation_List
+
+--
 -- Create tables
 --
+
+CREATE TABLE Corporation_List
+(	corp_id			INT CONSTRAINT pk_corporate_id PRIMARY KEY,
+	corp_name		NVARCHAR(30)
+);
+
 CREATE TABLE Customer_List
 (	customer_id		int CONSTRAINT pk_customerID PRIMARY KEY,
 	first_name		NVARCHAR(30) CONSTRAINT nn_customer_first_name NOT NULL,
@@ -114,24 +114,25 @@ CREATE TABLE Set_Release
 );
 --
 CREATE TABLE Card_List 
-(	card_name		NVARCHAR(60) CONSTRAINT LegalCardName NOT NULL,		
+(	card_id			INT CONSTRAINT pk_cardlist_card_id	PRIMARY KEY,
+	card_name		NVARCHAR(60) CONSTRAINT LegalCardName NOT NULL,		
 	mana_cost		NVARCHAR(20),
 	cmc				INT,
 	type_line		NVARCHAR(90),
 	oracle_text		NVARCHAR(MAX),
 	colors			NVARCHAR(1),
-	color_identity	NVARCHAR(1),
 	set_name		NVARCHAR(60) CONSTRAINT fk_card_list_set_name FOREIGN KEY
 		REFERENCES Set_Release(set_name),
 	collector_number	INT,
 	rarity			NVARCHAR(10),
 	artist			NVARCHAR(50),
-	CONSTRAINT pk_Card_Set_ID PRIMARY KEY (card_name, set_name),
+	--CONSTRAINT pk_Card_Set_ID PRIMARY KEY (card_name, set_name),
 	--set_release_date date CONSTRAINT fk_Set_Release_Date FOREIGN KEY (set_name) REFERENCES Set_Release(released_at)
 	);
 --
 CREATE TABLE Card_Event_Legality
-(	card_name		NVARCHAR(60) CONSTRAINT nn_legal_card_name NOT NULL,
+(	card_id			INT CONSTRAINT pk_CEL_card_id PRIMARY KEY,
+	card_name		NVARCHAR(60) CONSTRAINT nn_legal_card_name NOT NULL,
 	set_name		NVARCHAR(60) CONSTRAINT nn_legal_set_name NOT NULL,
 	standard		NVARCHAR(10),
 	future			NVARCHAR(10),
@@ -143,7 +144,7 @@ CREATE TABLE Card_Event_Legality
 	penny			NVARCHAR(10),
 	commander		NVARCHAR(10),
 	duel			NVARCHAR(10),
-	CONSTRAINT pk_card_set_id_event PRIMARY KEY (card_name, set_name)
+	--CONSTRAINT pk_card_set_id_event PRIMARY KEY (card_name, set_name)
 );
 
 CREATE TABLE Customer_Survey
@@ -163,48 +164,41 @@ CREATE TABLE Event_List
 	state			NVARCHAR(30),
 	city			NVARCHAR(30)
 );
---
-CREATE TABLE Card_Inventory
-(	card_id			INT CONSTRAINT pk_card_inventory_id PRIMARY KEY,
-	card_name		NVARCHAR(60) CONSTRAINT nn_inventory_card_name NOT NULL,
-	set_name		NVARCHAR(60) CONSTRAINT nn_inventory_set_name NOT NULL,
-	foil_tag		NVARCHAR(10),
-	quantity		INT,
-	CONSTRAINT fk_inventory_card_set_name FOREIGN KEY (card_name, set_name) 
-		REFERENCES Card_List(card_name, set_name),
-	CONSTRAINT fk_inventory_set_release FOREIGN KEY (set_name)
-		REFERENCES Set_Release(set_name),
-	CONSTRAINT fk_inventory_card_event_legality FOREIGN KEY (card_name, set_name)
-		REFERENCES Card_Event_Legality (card_name, set_name)
-);
---
-CREATE TABLE Agent_Transactions
-(	transaction_id	INT CONSTRAINT pk_agent_transaction_id PRIMARY KEY,
-	agent_id		INT CONSTRAINT fk_agent_id FOREIGN KEY
-		REFERENCES Agent_List(Agent_ID),
-	event_id		INT CONSTRAINT fk_agent_event_id FOREIGN KEY
-		REFERENCES Event_List(event_id),
-	card_id			INT CONSTRAINT fk_agent_card_id FOREIGN KEY
-		REFERENCES Card_Inventory(card_id),
-	price			MONEY CONSTRAINT nn_agent_price NOT NULL,
-	qty				INT CONSTRAINT nn_agent_qty NOT NULL
-);
 
 --
 CREATE TABLE Sales_Transactions
 (	sales_id		int CONSTRAINT pk_sell_id PRIMARY KEY,
 	customer_id		int CONSTRAINT fk_customer_sell_ID FOREIGN KEY
 		REFERENCES Customer_List(customer_id), 
-	card_id			int CONSTRAINT fk_card_sell_ID FOREIGN KEY
-		REFERENCES Card_Inventory(card_id),
+	card_id			int CONSTRAINT fk_card_ID FOREIGN KEY
+		REFERENCES Card_List(card_id),
+	agent_id		INT CONSTRAINT fk_agent_id FOREIGN KEY
+		REFERENCES Agent_List(agent_id),
+	event_id		INT CONSTRAINT fk_event_id FOREIGN KEY
+		REFERENCES Event_List(event_id),
 	price			money CONSTRAINT nn_sell_price NOT NULL,
-	sell_quantity	int CONSTRAINT nn_sell_qty NOT NULL,
-	sell_date		date CONSTRAINT nn_sell_date NOT NULL,
-	promotion_tag	NVARCHAR(15)
+	quantity	int CONSTRAINT nn_sell_qty NOT NULL,
+	date		date CONSTRAINT nn_sell_date NOT NULL,
+	foil_tag	NVARCHAR(15)
 );
 --
 -- Load table data
 --
+--untested
+EXECUTE (N'BULK INSERT Corporation_List FROM ''' + @data_path + N'Corporation List.csv'' 
+WITH (
+	CHECK_CONSTRAINTS,
+	CODEPAGE=''ACP'',
+	DATAFILETYPE = ''char'',
+	FIELDTERMINATOR= '','',
+	ROWTERMINATOR = ''\n'',
+	FIRSTROW = 2,
+	KEEPIDENTITY,
+	KEEPNULLS,
+	TABLOCK
+	);
+');
+
 --untested
 EXECUTE (N'BULK INSERT Customer_List FROM ''' + @data_path + N'Customer List.csv''
 WITH (
@@ -266,7 +260,7 @@ WITH (
 ');
 
 --Compiles
-EXECUTE (N'BULK INSERT Card_List FROM ''' + @data_path + N'Cards Only Simplified list.txt''
+EXECUTE (N'BULK INSERT Card_List FROM ''' + @data_path + N'Card Text Information.txt''
 WITH (
 	CHECK_CONSTRAINTS,
 	CODEPAGE=''ACP'',
@@ -308,35 +302,6 @@ WITH (
 	);
 ');
 --
-EXECUTE (N'BULK INSERT Card_Inventory FROM ''' + @data_path + N'Cards In Stock 1.1.2018.txt''
-WITH (
-	CHECK_CONSTRAINTS,
-	CODEPAGE=''ACP'',
-	DATAFILETYPE = ''char'',
-	FIELDTERMINATOR= ''\t'',
-	ROWTERMINATOR = ''\n'',
-	FIRSTROW = 2,
-	KEEPIDENTITY,
-	KEEPNULLS,
-	TABLOCK
-	);
-');
---
-EXECUTE (N'BULK INSERT Agent_Transactions FROM ''' + @data_path + N'Agent Transaction List.csv''
-WITH (
-	CHECK_CONSTRAINTS,
-	CODEPAGE=''ACP'',
-	DATAFILETYPE = ''char'',
-	FIELDTERMINATOR= '','',
-	ROWTERMINATOR = ''\n'',
-	FIRSTROW = 2,
-	KEEPIDENTITY,
-	KEEPNULLS,
-	TABLOCK
-	);
-');
-
---
 EXECUTE (N'BULK INSERT Sales_Transactions FROM ''' + @data_path + N'Sales Transactions.csv''
 WITH (
 	CHECK_CONSTRAINTS,
@@ -354,15 +319,13 @@ WITH (
 -- Verify Row Count in Tables.
 GO
 SET NOCOUNT ON
-SELECT 'Set_Release' AS "Table",	COUNT(*) AS "Rows"		  FROM Set_Release  UNION
-SELECT 'Agent_List',					COUNT(*) FROM Agent_List					UNION
+SELECT 'Agent_List' AS "Table"	,	COUNT(*) FROM Agent_List	AS "Rows"		UNION
 SELECT 'Card_list',					COUNT(*) FROM Card_List					    UNION
 SELECT 'Set_Release',				COUNT(*) FROM Set_Release					UNION
 SELECT 'Card Legality',				COUNT(*) FROM Card_Event_Legality			UNION
 SELECT 'Event_List',				COUNT(*) FROM Event_List					UNION
 SELECT 'Customer_Survey',			COUNT(*) FROM Customer_Survey				UNION
-SELECT 'Card Inventory',			COUNT(*) FROM Card_Inventory				UNION
-SELECT 'Agent Transactions',		COUNT(*) FROM Agent_Transactions			UNION
+SELECT 'Corporate ID',				COUNT(*) FROM Corporation_List				UNION
 SELECT 'Sell_Transactions',			COUNT(*) FROM Sales_Transactions				
 ORDER BY 1;
 SET NOCOUNT OFF
